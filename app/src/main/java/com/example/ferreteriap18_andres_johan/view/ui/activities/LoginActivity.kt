9bad5e1adcf9189e976af03b16fdf22b690b1043
Andres Salazar <andressalazar.alturo@gmail.com>
2021-12-02 19:52:19 -0500

    full guide 17/Sprint 4

---
+++ app/src/main/java/com/example/ferreteriap18_andres_johan/view/ui/activities/LoginActivity.kt
@@ -0,0 +1,63 @@
+package com.example.ferreteriap18_andres_johan.view.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ferreteriap18_andres_johan.R
+import com.example.ferreteriap18_andres_johan.databinding.ActivityLoginBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.Auth
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    val AUTH_REQUEST_CODE=1234
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var listener: FirebaseAuth.AuthStateListener
+    lateinit var providers: List<AuthUI.IdpConfig>
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener (listener)
+    }
+
+    override fun onStop() {
+        if (listener !=null)
+            firebaseAuth.removeAuthStateListener(listener)
+        super.onStop()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        providers= arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            // AuthUI.IdpConfig.GoogleBuilder().build(),
+            // AuthUI.IdpConfig.PhoneBuilder().build(),
+            // AuthUI.IdpConfig.AnonymousBuilder().build(),
+        )
+        val intent = Intent(this, MainActivity::class.java)
+        firebaseAuth= FirebaseAuth.getInstance()
+        listener= FirebaseAuth.AuthStateListener { p0 ->
+            val user= p0.currentUser
+            if (user!=null){
+                startActivity(intent)
+
+            }else{
+                startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .setLogo(R.drawable.logo_large)
+                    .setTheme(R.style.FullScreenDialogStyle)
+                    .build(),AUTH_REQUEST_CODE)
+            }
+        }
+
+    }
+
+}
